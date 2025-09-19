5f0b8a4f4f6ed041261dae92e84a3bb0adab8018
Pranjal Kumar <kpranjal219@gmail.com>
2025-09-20 00:11:44 +0530

    rotes.ts updated for deployment

--- app/api/add/route.ts
+++ app/api/add/route.ts
@@ -4,9 +4,9 @@ import { NextResponse } from "next/server";
 import admin from "firebase-admin"
 
 export function GET(request: Request) {
-    return NextResponse.json({
-        message: "Backend in working bhai!!!!!!!"
-    })
+  return NextResponse.json({
+    message: "Backend in working bhai!!!!!!!"
+  })
 }
 
 
@@ -16,28 +16,32 @@ export function GET(request: Request) {
 //     return NextResponse.json({success: true, data: "yes"})
 // }
 if (!admin.apps.length) {
-    admin.initializeApp({
-      credential: admin.credential.cert({
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-        clientEmail: process.env.NEXT_FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.NEXT_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-      }),
-    });
-  }
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      clientEmail: process.env.NEXT_FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.NEXT_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
+  });
+}
 
-  const db = admin.firestore();
+const db = admin.firestore();
 
-  export async function POST(req: Request) {
-    try {
-      const body = await req.json();
-      console.log("Request body:", body);
-  
-      const docRef = await db.collection("users").add(body);
-  
-      return NextResponse.json({ message: "User onboarded successfully", id: docRef.id }, { status: 200 });
-    } catch (error: any) {
-      console.error(error);
-      return NextResponse.json({ error: "Failed to onboard Users" }, { status: 500 });
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    console.log("Request body:", body);
+
+    const docRef = await db.collection("users").add(body);
+
+    return NextResponse.json({ message: "User onboarded successfully", id: docRef.id }, { status: 200 });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error("Error message:", error.message);
+    } else {
+      console.error("Unexpected error:", error);
     }
+
+    return NextResponse.json({ error: "Failed to onboard Users" }, { status: 500 });
   }
-  
+}
